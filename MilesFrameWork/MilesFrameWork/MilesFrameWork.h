0686ea570606c80bc5af41f8ee02723533f33b6e
miles <qq867327466@163.com>
2020-09-14 17:57:06 +0800

    first push

---
+++ MilesFrameWork/MilesFrameWork/MilesFrameWork.h
@@ -0,0 +1,19 @@
+//
+//  MilesFrameWork.h
+//  MilesFrameWork
+//
+//  Created by 徐文杰 on 2020/9/14.
+//  Copyright © 2020 ruilisi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MilesFrameWork.
+FOUNDATION_EXPORT double MilesFrameWorkVersionNumber;
+
+//! Project version string for MilesFrameWork.
+FOUNDATION_EXPORT const unsigned char MilesFrameWorkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MilesFrameWork/PublicHeader.h>
+
+
---
+++ MilesFrameWork/MilesFrameWork/WebSocketHelper.swift
@@ -0,0 +1,17 @@
+//
+//  WebSocketHelper.swift
+//  MilesFrameWork
+//
+//  Created by 徐文杰 on 2020/9/14.
+//  Copyright © 2020 ruilisi. All rights reserved.
+//
+
+import Foundation
+
+open class WebSocketHelper {
+    private init() {}
+    
+    public static func doSomething() -> String {
+        return "Yes! You got it!"
+    }
+}
